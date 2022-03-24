29b1e3be36a9eaac5014ef979db0b96d3df0f40c
Felix <jonfelixrico@gmail.com>
2022-03-25 02:30:11 +0800

    Create endpoints for controlling switch modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common'
 import { ArduinoController } from './controllers/arduino/arduino.controller'
 import { UnknownDevicesController } from './controllers/api/unknown-devices/unknown-devices.controller'
 import { DevicesController } from './controllers/api/devices/devices.controller'
+import { SwitchController } from './controllers/api/device-modules/switch/switch.controller'
 
 @Module({
   imports: [],
-  controllers: [ArduinoController, UnknownDevicesController, DevicesController],
+  controllers: [
+    ArduinoController,
+    UnknownDevicesController,
+    DevicesController,
+    SwitchController,
+  ],
   providers: [],
 })
 export class AppModule {}

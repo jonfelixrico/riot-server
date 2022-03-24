e63f607c845b6a4a02afcd99de99a1b9d5f50a4b
Felix <jonfelixrico@gmail.com>
2022-03-25 01:55:53 +0800

    Add sub routes

--- src/controllers/api/devices/devices.controller.ts
+++ src/controllers/api/devices/devices.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, Put } from '@nestjs/common'
 
 @Controller('api/devices')
-export class DevicesController {}
+export class DevicesController {
+  @Get()
+  listDevices() {
+    throw new Error('noop')
+  }
+
+  @Get(':deviceId')
+  getDevice(@Param('deviceId') deviceId: string) {
+    throw new Error('noop')
+  }
+
+  @Put(':deviceId')
+  updateDeviceModules(@Param('deviceId') deviceId: string) {
+    throw new Error('noop')
+  }
+
+  @Put(':deviceId/modules/:moduleId')
+  updateDeviceModule(
+    @Param('deviceId') deviceId: string,
+    @Param('moduleId') moduleId: string,
+  ) {
+    throw new Error('noop')
+  }
+}

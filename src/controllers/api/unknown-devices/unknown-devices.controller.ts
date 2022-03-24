c71fd5392b7dd91efbc403431fc314d1137f9c10
Felix <jonfelixrico@gmail.com>
2022-03-25 02:00:25 +0800

    Create subroutes for the devices/unknown route

--- src/controllers/api/unknown-devices/unknown-devices.controller.ts
+++ src/controllers/api/unknown-devices/unknown-devices.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, Post } from '@nestjs/common'
 
 @Controller('api/devices/unknown')
-export class UnknownDevicesController {}
+export class UnknownDevicesController {
+  @Get()
+  getUnknownDevices() {
+    throw new Error('noop')
+  }
+
+  @Get('deviceId')
+  getUnknownDevice(@Param('deviceId') deviceId: string) {
+    throw new Error('noop')
+  }
+
+  @Post('deviceId')
+  acknowledgeDevice(@Param('deviceId') deviceId: string) {
+    throw new Error('noop')
+  }
+}

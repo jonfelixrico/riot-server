ff7b501d3e126961fc46376e67d8d820881cc036
Felix <jonfelixrico@gmail.com>
2022-03-25 00:49:01 +0800

    Inject the services

--- src/controllers/arduino/arduino.controller.ts
+++ src/controllers/arduino/arduino.controller.ts
@@ -1,9 +1,18 @@
 import { Body, Controller, Get, Param, Put } from '@nestjs/common'
+import { ActuatorService } from 'src/services/actuator-service.abstract'
+import { DeviceService } from 'src/services/device-service.abstract'
+import { SensorService } from 'src/services/sensor-service.abstract'
 import { PushSensorReadingsDto } from './push-sensor-readings.dto'
 import { RegisterDeviceDto } from './register-device.dto'
 
 @Controller('arduino')
 export class ArduinoController {
+  constructor(
+    private actuatorSvc: ActuatorService,
+    private sensorService: SensorService,
+    private deviceService: DeviceService,
+  ) {}
+
   @Get('/:id')
   getActuatorState(@Param('id') id: string) {
     throw new Error('noop')

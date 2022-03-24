2c31ffe0b509db0104df6524121bfd2175b772ed
Felix <jonfelixrico@gmail.com>
2022-03-25 01:20:47 +0800

    Create registration guards for arduino

---
+++ src/controllers/guards/arduino-registration.guard.spec.ts
@@ -0,0 +1,7 @@
+import { ArduinoRegistrationGuard } from './arduino-registration.guard'
+
+describe('ArduinoRegistrationGuard', () => {
+  it('should be defined', () => {
+    expect(new ArduinoRegistrationGuard()).toBeDefined()
+  })
+})

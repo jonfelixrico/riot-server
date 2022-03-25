63566cb969ca353ca9fe5873e39dca2bd2def5fb
Felix <jonfelixrico@gmail.com>
2022-03-25 22:22:40 +0800

    Create the responder

--- src/services/actuator-query/actuator-query.module.ts
+++ src/services/actuator-query/actuator-query.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ActuatorStateFetcherService } from './actuator-state-fetcher/actuator-state-fetcher.service'
+import { ActuatorStateResponderService } from './actuator-state-responder/actuator-state-responder.service'
 
 @Module({
-  providers: [ActuatorStateFetcherService],
+  providers: [ActuatorStateFetcherService, ActuatorStateResponderService],
   exports: [ActuatorStateFetcherService],
 })
 export class ActuatorQueryModule {}

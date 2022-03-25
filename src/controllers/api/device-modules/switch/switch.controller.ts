3da3fa139fa537b174201bc11cd45c906281b2cb
Felix <jonfelixrico@gmail.com>
2022-03-25 21:39:44 +0800

    Adjust the controller paths, add comments

--- src/controllers/api/device-modules/switch/switch.controller.ts
+++ src/controllers/api/device-modules/switch/switch.controller.ts
@@ -4,6 +4,11 @@ import { SwitchScheduleDto } from './switch-schedule.dto'
 
 @Controller('api/devices/:deviceId/modules/switch')
 export class SwitchController {
+  /**
+   * Gets the full details of a switch.
+   * @param deviceId
+   * @param switchId
+   */
   @Get(':switchId')
   getDetails(
     @Param('deviceId') deviceId: string,
@@ -12,14 +17,12 @@ export class SwitchController {
     throw new Error('noop')
   }
 
-  @Get(':switchId/state')
-  getState(
-    @Param('deviceId') deviceId: string,
-    @Param('switchId') switchId: string,
-  ) {
-    throw new Error('noop')
-  }
-
+  /**
+   * Updates the schedule of a switch.
+   * @param deviceId
+   * @param switchId
+   * @param schedule
+   */
   @Put(':switchId')
   setSchedule(
     @Param('deviceId') deviceId: string,
@@ -29,7 +32,26 @@ export class SwitchController {
     throw new Error('noop')
   }
 
-  @Put(':switchId/override')
+  /**
+   * Gets the current state of a switch.
+   * @param deviceId
+   * @param switchId
+   */
+  @Get(':switchId/state')
+  getState(
+    @Param('deviceId') deviceId: string,
+    @Param('switchId') switchId: string,
+  ) {
+    throw new Error('noop')
+  }
+
+  /**
+   * Overrides the state of a switch.
+   * @param deviceId
+   * @param switchId
+   * @param override
+   */
+  @Put(':switchId/state')
   setOverride(
     @Param('deviceId') deviceId: string,
     @Param('switchId') switchId: string,
@@ -38,7 +60,12 @@ export class SwitchController {
     throw new Error('noop')
   }
 
-  @Delete(':switchId/override')
+  /**
+   * Clears the override of a switch.
+   * @param deviceId
+   * @param switchId
+   */
+  @Delete(':switchId/state')
   clearOverride(
     @Param('deviceId') deviceId: string,
     @Param('switchId') switchId: string,

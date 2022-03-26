d3874c1fbc6c71a577dab567e443aa6019b4c1f0
Felix <jonfelixrico@gmail.com>
2022-03-26 16:26:26 +0800

    Adjust the module model as ModuleConfig

--- src/mongoose/models/module-config.mongo-model.ts
+++ src/mongoose/models/module-config.mongo-model.ts
@@ -1,11 +1,12 @@
 import { Schema, Connection } from 'mongoose'
-import { DeviceModule } from 'src/services/device-service.abstract'
+import type { DeviceModule } from './device.mongo-model'
 
 const moduleSchema = new Schema<DeviceModule>({
   id: String,
   type: String,
+  lastUpdateDt: Date,
 })
 
 export default function (conn: Connection) {
-  return conn.model('Module', moduleSchema)
+  return conn.model('ModuleConfig', moduleSchema)
 }

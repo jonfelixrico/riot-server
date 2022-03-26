0128b053bb6dee73d4aa80776ebcd228a2ff91ae
Felix <jonfelixrico@gmail.com>
2022-03-26 17:36:55 +0800

    Move UknownDevicesController

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,10 @@ import { Module } from '@nestjs/common'
 import { ArduinoController } from './controllers/arduino/arduino.controller'
 import { ActuatorQueryModule } from './services/actuator-query/actuator-query.module'
 import { MongooseModule } from './mongoose/mongoose.module'
-import { UnknownDevicesController } from './controllers/api/unknown-devices/unknown-devices.controller'
 import { DeviceControllersModule } from './controllers/device-controllers/device-controllers.module'
 
 @Module({
   imports: [ActuatorQueryModule, MongooseModule, DeviceControllersModule],
-  controllers: [
-    ArduinoController,
-    // UnknownDevicesController
-  ],
+  controllers: [ArduinoController],
 })
 export class AppModule {}
--- src/controllers/device-controllers/device-controllers.module.ts
+++ src/controllers/device-controllers/device-controllers.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common'
 import { SwitchController } from './switch/switch.controller'
 import { DevicesController } from './devices/devices.controller'
+import { UnknownDevicesController } from './unknown-devices/unknown-devices.controller'
 
 @Module({
-  controllers: [DevicesController, SwitchController],
+  controllers: [
+    DevicesController,
+    SwitchController,
+    //
+    UnknownDevicesController,
+  ],
 })
 export class DeviceControllersModule {}

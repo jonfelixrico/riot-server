095f5144a3f9338dbcc11c6da19148d8559f1b9d
Felix <jonfelixrico@gmail.com>
2022-03-26 22:25:37 +0800

    Add initial impl in SwitchImplService

--- src/services/specialized-devices/switch/switch-impl/switch-impl.service.ts
+++ src/services/specialized-devices/switch/switch-impl/switch-impl.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import {
+  DeviceModel,
+  SwitchConfigModel,
+} from 'src/mongoose/models/mongoose-models.types'
+import {
+  DEVICE_MODEL,
+  SWTICH_CONFIG_MODEL,
+} from 'src/mongoose/mongoose.di-tokens'
 import {
   DailySchedule,
   HourlySchedule,
@@ -10,8 +18,38 @@ import {
 
 @Injectable()
 export class SwitchImplService extends SwitchModuleService {
-  getState(deviceId: string, moduleId: string): Promise<SwitchState> {
-    throw new Error('Method not implemented.')
+  constructor(
+    @Inject(DEVICE_MODEL) private devices: DeviceModel,
+    @Inject(SWTICH_CONFIG_MODEL) private switchConfigs: SwitchConfigModel,
+  ) {
+    super()
+  }
+
+  private async fetch(deviceId: string, moduleId: string) {
+    const device = await this.devices.findOne({
+      id: deviceId,
+    })
+
+    if (device) {
+      return null
+    }
+
+    const dModule = device.modules.find(({ id }) => id === moduleId)
+    if (!dModule) {
+      console.warn(
+        'SwitchImplService: device was found but module ref was not found',
+      )
+      return null
+    }
+
+    return await this.switchConfigs.findById(dModule.config)
+  }
+
+  async getState(deviceId: string, moduleId: string): Promise<SwitchState> {
+    const record = this.fetch(deviceId, moduleId)
+    if (!record) {
+      return null
+    }
   }
 
   setSchedule(

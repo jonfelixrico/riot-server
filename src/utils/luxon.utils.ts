e29be6b5a67f12a05de031f5b8550fd64205b981
Felix <jonfelixrico@gmail.com>
2022-03-26 20:35:23 +0800

    Add comments

--- src/utils/luxon.utils.ts
+++ src/utils/luxon.utils.ts
@@ -1,5 +1,13 @@
 import { DateTime } from 'luxon'
 
+/**
+ * Checks if a date is within a given range. Inclusive.
+ *
+ * @param date The date date to compare against the range.
+ * @param from The start of the range.
+ * @param to The end of the range.
+ * @returns True if is between, false if otherwise.
+ */
 export function isBetween(date: DateTime, from: DateTime, to: DateTime) {
-  return date.diff(from).milliseconds >= 0 && to.diff(from).milliseconds >= 0
+  return date.diff(from).milliseconds >= 0 && to.diff(date).milliseconds >= 0
 }

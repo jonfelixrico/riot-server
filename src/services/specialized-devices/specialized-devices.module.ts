692b12bb6db50296edaeeeaadec90245b7c420b8
Felix <jonfelixrico@gmail.com>
2022-03-26 17:57:49 +0800

    Apply module adjustments to SpecializedDevices

--- src/services/specialized-devices/specialized-devices.module.ts
+++ src/services/specialized-devices/specialized-devices.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ActuatorQueryModule } from '../actuator-query/actuator-query.module'
 import { SwitchImplService } from './switch/switch-impl/switch-impl.service'
+import { SwitchModuleService } from './switch/switch-module-service.abstract'
 
 @Module({
-  providers: [SwitchImplService],
+  imports: [ActuatorQueryModule],
+
+  providers: [
+    SwitchImplService,
+    {
+      useExisting: SwitchImplService,
+      provide: SwitchModuleService,
+    },
+  ],
+
+  exports: [SwitchModuleService],
 })
 export class SpecializedDevicesModule {}

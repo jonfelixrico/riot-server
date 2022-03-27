47dc20b2ffda420438ef04a7a8f9001e069f283b
Felix <jonfelixrico@gmail.com>
2022-03-27 13:32:18 +0800

    Create impl class for ActuatorService

--- src/services/generic-devices/generic-device.module.ts
+++ src/services/generic-devices/generic-device.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { DeviceImplService } from './device-impl/device-impl.service'
 import { DeviceService } from './device-service.abstract'
+import { ActuatorImplService } from './actuator-impl/actuator-impl.service'
+import { ActuatorService } from './actuator-service.abstract'
 
 @Module({
   imports: [],
@@ -9,6 +11,12 @@ import { DeviceService } from './device-service.abstract'
     {
       provide: DeviceService,
       useExisting: DeviceImplService,
+    },
+
+    ActuatorImplService,
+    {
+      provide: ActuatorService,
+      useExisting: ActuatorImplService,
     },
   ],
 })

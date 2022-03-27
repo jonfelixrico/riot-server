f954dab637f16788a2017468ff3dffa1838e90b4
Felix <jonfelixrico@gmail.com>
2022-03-27 13:28:54 +0800

    Create DeviceImplService

---
+++ src/services/generic-devices/device-impl/device-impl.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { DeviceImplService } from './device-impl.service'
+
+describe('DeviceImplService', () => {
+  let service: DeviceImplService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [DeviceImplService],
+    }).compile()
+
+    service = module.get<DeviceImplService>(DeviceImplService)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
+})
--- src/services/generic-devices/generic-device.module.ts
+++ src/services/generic-devices/generic-device.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common'
+import { DeviceImplService } from './device-impl/device-impl.service'
+import { DeviceService } from './device-service.abstract'
 
 @Module({
   imports: [],
+  providers: [
+    DeviceImplService,
+    {
+      provide: DeviceService,
+      useExisting: DeviceImplService,
+    },
+  ],
 })
 export class GenericDevicesModule {}

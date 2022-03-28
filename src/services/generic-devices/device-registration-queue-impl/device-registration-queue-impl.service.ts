69d1e28f716a148630b0e4b5a2cb1cde04ea4ab5
Felix <jonfelixrico@gmail.com>
2022-03-29 02:30:33 +0800

    Push implementation in DeviceRegistrationServiceImpl

--- src/services/generic-devices/device-registration-queue-impl/device-registration-queue-impl.service.ts
+++ src/services/generic-devices/device-registration-queue-impl/device-registration-queue-impl.service.ts
@@ -1,23 +1,52 @@
-import { Injectable } from '@nestjs/common'
+import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { sortBy } from 'lodash'
 import {
   DeviceQuery,
   DeviceRegistrationQueueService,
   DeviceToQueue,
   QueuedDevice,
 } from '../device-registration-queue-service.abstract'
+import { DeviceService } from '../device-service.abstract'
+
+const convertToKey = ({
+  firmwareVersion,
+  deviceId,
+}: {
+  firmwareVersion: string
+  deviceId: string
+}) => `${deviceId}/${firmwareVersion}`
 
 @Injectable()
 export class DeviceRegistrationQueueImplService extends DeviceRegistrationQueueService {
-  getQueueItem(input: DeviceQuery): Promise<QueuedDevice> {
-    throw new Error('Method not implemented.')
+  private inQueue: Record<string, QueuedDevice> = {}
+
+  constructor(
+    @Inject(forwardRef(() => DeviceService)) private deviceSvc: DeviceService,
+  ) {
+    super()
   }
-  flagForQueue(input: DeviceToQueue): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async getQueueItem(input: DeviceQuery): Promise<QueuedDevice> {
+    return this.inQueue[convertToKey(input)]
   }
-  removeFromQueue(input: DeviceQuery): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async flagForQueue(input: DeviceToQueue): Promise<void> {
+    if (await this.deviceSvc.doesDeviceExist(input)) {
+      throw new Error('already registered')
+    }
+
+    this.inQueue[convertToKey(input)] = {
+      ...input,
+      lastQueueDt: new Date(),
+    }
   }
-  getQueueItems(): Promise<QueuedDevice[]> {
-    throw new Error('Method not implemented.')
+
+  async removeFromQueue(input: DeviceQuery): Promise<void> {
+    delete this.inQueue[convertToKey(input)]
+  }
+
+  async getQueueItems(): Promise<QueuedDevice[]> {
+    const values = Object.values(this.inQueue)
+    return sortBy(values, ['deviceId', 'version'])
   }
 }

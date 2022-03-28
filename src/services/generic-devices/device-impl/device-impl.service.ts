34af4fa9c4865f8071e1311242ed34cabbe46ca9
Felix <jonfelixrico@gmail.com>
2022-03-29 02:38:10 +0800

    Implement placeholders for the device implementation

--- src/services/generic-devices/device-impl/device-impl.service.ts
+++ src/services/generic-devices/device-impl/device-impl.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common'
+import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { DeviceRegistrationQueueService } from '../device-registration-queue-service.abstract'
+import { Device, DeviceModule, DeviceService } from '../device-service.abstract'
+
+interface DeviceQuery {
+  deviceId: string
+  firmwareVersion: string
+}
+
+interface DeviceToRegister extends DeviceQuery {
+  modules: DeviceModule[]
+}
 
 @Injectable()
-export class DeviceImplService {}
+export class DeviceImplService extends DeviceService {
+  constructor(
+    @Inject(forwardRef(() => DeviceRegistrationQueueService))
+    private regSvc: DeviceRegistrationQueueService,
+  ) {
+    super()
+  }
+
+  async bumpHeartbeat(input: DeviceQuery): Promise<void> {
+    if (!(await this.doesDeviceExist(input))) {
+      throw new Error('device not found')
+    }
+
+    // TODO update db record
+  }
+
+  async getDevice(input: DeviceQuery): Promise<Device> {
+    // TODO return record
+    throw new Error('Method not implemented.')
+  }
+
+  async doesDeviceExist(input: DeviceQuery): Promise<boolean> {
+    // TODO return record
+    throw new Error('Method not implemented.')
+  }
+
+  async registerDevice(input: DeviceToRegister) {
+    if (await this.doesDeviceExist(input)) {
+      throw new Error('device is already registered')
+    }
+
+    // TODO do registration
+    throw new Error('Method not implemented.')
+  }
+
+  getDevices(): Promise<Device[]> {
+    // TODO fetch records
+    throw new Error('Method not implemented.')
+  }
+}

78118e69c64598cab398011e906e193a9361de45
Felix <jonfelixrico@gmail.com>
2022-03-30 00:30:38 +0800

    Implement DeviceImpl

--- src/services/generic-devices/device-impl/device-impl.service.ts
+++ src/services/generic-devices/device-impl/device-impl.service.ts
@@ -1,6 +1,10 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { DEVICE_MODEL } from 'src/mongoose/mongoose.di-tokens'
 import { DeviceRegistrationQueueService } from '../device-registration-queue-service.abstract'
 import { Device, DeviceModule, DeviceService } from '../device-service.abstract'
+import { Model } from 'mongoose'
+import { MongooseDevice } from 'src/mongoose/models/device.mongoose-model'
+import { SwitchModuleService } from 'src/services/specialized-devices/switch/switch-module-service.abstract'
 
 interface DeviceQuery {
   deviceId: string
@@ -16,26 +20,36 @@ export class DeviceImplService extends DeviceService {
   constructor(
     @Inject(forwardRef(() => DeviceRegistrationQueueService))
     private regSvc: DeviceRegistrationQueueService,
+    @Inject(DEVICE_MODEL) private deviceModel: Model<MongooseDevice>,
+    private switchSvc: SwitchModuleService,
   ) {
     super()
   }
 
+  private async fetch({ deviceId, firmwareVersion }: DeviceQuery) {
+    return await this.deviceModel.findOne({ id: deviceId, firmwareVersion })
+  }
+
   async bumpHeartbeat(input: DeviceQuery): Promise<void> {
-    if (!(await this.doesDeviceExist(input))) {
+    const device = await this.fetch(input)
+    if (!device) {
       throw new Error('device not found')
     }
 
-    // TODO update db record
+    // TODO create date provider
+    device.lastHeartbeatDt = new Date()
+    await device.save()
   }
 
   async getDevice(input: DeviceQuery): Promise<Device> {
-    // TODO return record
-    throw new Error('Method not implemented.')
+    return await this.fetch(input)
   }
 
-  async doesDeviceExist(input: DeviceQuery): Promise<boolean> {
-    // TODO return record
-    throw new Error('Method not implemented.')
+  async doesDeviceExist({
+    deviceId,
+    firmwareVersion,
+  }: DeviceQuery): Promise<boolean> {
+    return !!(await this.deviceModel.exists({ id: deviceId, firmwareVersion }))
   }
 
   async registerDevice(input: DeviceToRegister) {
@@ -43,12 +57,22 @@ export class DeviceImplService extends DeviceService {
       throw new Error('device is already registered')
     }
 
-    // TODO do registration
-    throw new Error('Method not implemented.')
+    const queued = await this.regSvc.getQueueItem(input)
+    if (!queued) {
+      throw new Error('queued item is not found')
+    }
+
+    await this.deviceModel.create(input)
+    for (const { id } of queued.modules) {
+      // TODO change once we get other types
+      await this.switchSvc.initalizeSwitchConfig({
+        ...input,
+        moduleId: id,
+      })
+    }
   }
 
-  getDevices(): Promise<Device[]> {
-    // TODO fetch records
-    throw new Error('Method not implemented.')
+  async getDevices(): Promise<Device[]> {
+    return await this.deviceModel.find().exec()
   }
 }

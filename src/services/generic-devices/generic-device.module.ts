a5bf389efbd7316a1a097b53539f946a7a764c56
Felix <jonfelixrico@gmail.com>
2022-03-30 00:43:23 +0800

    Create impl for ModuleStateService

--- src/services/generic-devices/generic-device.module.ts
+++ src/services/generic-devices/generic-device.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from 'src/mongoose/mongoose.module'
 import { DeviceRegistrationQueueImplService } from './device-registration-queue-impl/device-registration-queue-impl.service'
 import { DeviceRegistrationQueueService } from './device-registration-queue-service.abstract'
 import { SpecializedDevicesModule } from '../specialized-devices/specialized-devices.module'
+import { ModuleStateImplService } from './module-state-impl/module-state-impl.service'
+import { ModuleStateService } from './module-state-service.abstract'
 
 @Module({
   imports: [MongooseModule, SpecializedDevicesModule],
@@ -20,6 +22,12 @@ import { SpecializedDevicesModule } from '../specialized-devices/specialized-dev
     {
       provide: DeviceRegistrationQueueService,
       useExisting: DeviceRegistrationQueueImplService,
+    },
+
+    ModuleStateImplService,
+    {
+      provide: ModuleStateService,
+      useExisting: ModuleStateImplService,
     },
   ],
 

a4f46146eb93f6f115f1338282651e093cc78693
Felix <jonfelixrico@gmail.com>
2022-03-30 22:02:26 +0800

    Create interface version

--- src/services/generic-devices/module-state-service.abstract.ts
+++ src/services/generic-devices/module-state-service.abstract.ts
@@ -1,4 +1,22 @@
-export abstract class ModuleStateService {
+export abstract class ModuleStateService implements ModuleStateProvider {
+  abstract getState(query: {
+    moduleId: string
+    deviceId: string
+    firmwareVersion: string
+  }): Promise<unknown>
+
+  abstract getStates(query: {
+    deviceId: string
+    firmwareVersion: string
+  }): Promise<Record<string, unknown>>
+}
+
+/**
+ * Dependency injection token for {@link ModuleStateProvider}.
+ */
+export const MODULE_STATE_PROVIDER = Symbol('module state provider')
+
+export abstract class ModuleStateProvider {
   abstract getState(query: {
     moduleId: string
     deviceId: string

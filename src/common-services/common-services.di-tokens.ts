c4b1707c88277d38e1298b90f9f0c565700c04cd
Felix <jonfelixrico@gmail.com>
2022-03-30 22:34:51 +0800

    Remove di-tokens files

--- src/common-services/common-services.di-tokens.ts
+++
@@ -1,1 +0,0 @@
-export { DATETIME_PROVIDER as TIME_PROVIDER } from './time-provider.interface'
--- src/services/generic-devices/generic-devices.di-tokens.ts
+++
@@ -1,2 +0,0 @@
-export { DEVICE_REGISTRATION_QUEUE } from './device-registration-queue.interface'
-export { DEVICE_MANAGER } from './device-manager.interface'

16878d0c2bbd0a1b2439af7b4f42addde4a8e148
Felix <jonfelixrico@gmail.com>
2022-03-30 20:42:38 +0800

    Import the CommonServices module

--- src/services/specialized-devices/specialized-devices.module.ts
+++ src/services/specialized-devices/specialized-devices.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CommonServicesModule } from 'src/common-services/common-services.module'
 import { MongooseModule } from 'src/mongoose/mongoose.module'
 import { SwitchImplService } from './switch/switch-impl/switch-impl.service'
 import { SwitchModuleService } from './switch/switch-module-service.abstract'
 
 @Module({
-  imports: [MongooseModule],
+  imports: [MongooseModule, CommonServicesModule],
 
   providers: [
     SwitchImplService,

5384b16ef612f9549e2838fca29c3e0c6018b07e
Felix <jonfelixrico@gmail.com>
2022-03-31 01:57:17 +0800

    Create registration flow e2e test

--- test/arduino.e2e-spec.ts
+++ test/arduino.e2e-spec.ts
@@ -1,24 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
-import { ArduinoControllersModule } from '@app/controllers/arduino-controllers/arduino-controllers.module'
+import { AppModule } from '@app/app.module'
+import { uniqueId } from 'lodash'
+import { DeviceModule } from '@app/services/generic-devices/device-manager.interface'
+import { QueuedDevice } from '@app/services/generic-devices/device-registration-queue.interface'
 
-describe('AppController (e2e)', () => {
+describe('registration flow', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ArduinoControllersModule],
+      imports: [AppModule],
     }).compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
   })
 
-  test('processEmit', () => {
+  const deviceId = uniqueId()
+  const version = '1'
+  const testDevice: DeviceModule = {
+    id: 'device-1',
+    type: 'switch',
+  }
+
+  it('should return 404 for unregistered devices', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .put(`/arduino/${deviceId}/version/${version}`)
+      .expect(404)
+  })
+
+  it('should acccept registration from unregistered devcies', () => {
+    return request(app.getHttpServer())
+      .post(`/arduino/${deviceId}/version/${version}`)
+      .send([testDevice])
+      .expect(202)
+  })
+
+  it('should be in registration list', async () => {
+    const promise: Promise<{ body: QueuedDevice }> = request(
+      app.getHttpServer(),
+    )
+      .get(`/api/device/unregistered/${deviceId}/version/${version}`)
       .expect(200)
-      .expect('Hello World!')
+
+    const { body } = await promise
+
+    expect(body.deviceId).toEqual(deviceId)
+    expect(body.firmwareVersion).toEqual(version)
+    expect(body.modules).toContainEqual(testDevice)
+  })
+
+  it('should register sucesssfully', async () => {
+    await request(app.getHttpServer())
+      .post(`/api/device/unregistered/${deviceId}/version/${version}`)
+      .expect(201)
   })
 })

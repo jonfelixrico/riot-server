954945c0e80b4b3917759e86f3466c6adeb72047
Felix <jonfelixrico@gmail.com>
2022-03-30 21:56:21 +0800

    Rename to DeviceRegistrationQueue

--- src/services/generic-devices/device-registration-queue-service.abstract.ts
+++ src/services/generic-devices/device-registration-queue-service.abstract.ts
@@ -13,7 +13,9 @@ export interface QueuedDevice extends DeviceToQueue {
   lastQueueDt: Date
 }
 
-export abstract class DeviceRegistrationQueueService {
+export abstract class DeviceRegistrationQueueService
+  implements DeviceRegistrationQueue
+{
   abstract flagForQueue(input: DeviceToQueue): Promise<void>
 
   abstract removeFromQueue(input: DeviceQuery): Promise<void>
@@ -21,4 +23,19 @@ export abstract class DeviceRegistrationQueueService {
   abstract getQueueItems(): Promise<QueuedDevice[]>
 
   abstract getQueueItem(input: DeviceQuery): Promise<QueuedDevice>
+}
+
+/**
+ * Dependency injection token for {@link DeviceRegistrationQueue}.
+ */
+export const DeviceRegistrationQueue = Symbol('registration queue')
+
+export interface DeviceRegistrationQueue {
+  flagForQueue(input: DeviceToQueue): Promise<void>
+
+  removeFromQueue(input: DeviceQuery): Promise<void>
+
+  getQueueItems(): Promise<QueuedDevice[]>
+
+  getQueueItem(input: DeviceQuery): Promise<QueuedDevice>
 }

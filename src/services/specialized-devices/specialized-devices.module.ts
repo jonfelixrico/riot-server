0ecb64e1153ecbcf3588ac785d9859ffe9fb8105
Felix <jonfelixrico@gmail.com>
2022-03-30 20:48:06 +0800

    Completely remove SwitchModuleService

--- src/services/specialized-devices/specialized-devices.module.ts
+++ src/services/specialized-devices/specialized-devices.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common'
 import { CommonServicesModule } from 'src/common-services/common-services.module'
 import { MongooseModule } from 'src/mongoose/mongoose.module'
 import { SwitchImplService } from './switch/switch-impl/switch-impl.service'
-import {
-  SwitchModuleService,
-  SWITCH_MANAGER,
-} from './switch/switch-manager.interface'
+import { SWITCH_MANAGER } from './switch/switch-manager.interface'
 
 @Module({
   imports: [MongooseModule, CommonServicesModule],
 
   providers: [
     SwitchImplService,
-    {
-      useExisting: SwitchImplService,
-      provide: SwitchModuleService,
-    },
     {
       useExisting: SwitchImplService,
       provide: SWITCH_MANAGER,

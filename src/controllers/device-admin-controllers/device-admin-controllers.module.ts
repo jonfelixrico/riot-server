df6444fc81fe5e8aca7d269e9d51a63825949c50
Felix <jonfelixrico@gmail.com>
2022-03-30 22:44:54 +0800

    Import SpecializedDevicesModule

--- src/controllers/device-admin-controllers/device-admin-controllers.module.ts
+++ src/controllers/device-admin-controllers/device-admin-controllers.module.ts
@@ -3,6 +3,7 @@ import { DevicesController } from './controllers/devices/devices.controller'
 import { GenericDevicesModule } from 'src/services/generic-devices/generic-device.module'
 import { SwitchController } from './controllers/switch/switch.controller'
 import { DeviceRegistrationController } from './controllers/device-registration/device-registration.controller'
+import { SpecializedDevicesModule } from 'src/services/specialized-devices/specialized-devices.module'
 
 /**
  * This modules holds the controllers responsible for managing/configuring devices.
@@ -16,6 +17,6 @@ import { DeviceRegistrationController } from './controllers/device-registration/
     // UnknownDevicesController,
   ],
 
-  imports: [GenericDevicesModule],
+  imports: [GenericDevicesModule, SpecializedDevicesModule],
 })
 export class DeviceAdminControllersModule {}

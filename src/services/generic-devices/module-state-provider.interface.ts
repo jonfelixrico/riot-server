cdc5d57e1c10a4dad26314fd5f980189c809a0e5
Felix <jonfelixrico@gmail.com>
2022-03-31 21:31:52 +0800

    Import ModuleStateProvider

--- src/services/generic-devices/module-state-provider.interface.ts
+++ src/services/generic-devices/module-state-provider.interface.ts
@@ -1,16 +1,3 @@
-export abstract class ModuleStateService implements ModuleStateProvider {
-  abstract getState(query: {
-    moduleId: string
-    deviceId: string
-    firmwareVersion: string
-  }): Promise<unknown>
-
-  abstract getStates(query: {
-    deviceId: string
-    firmwareVersion: string
-  }): Promise<Record<string, unknown>>
-}
-
 /**
  * Dependency injection token for {@link ModuleStateProvider}.
  */

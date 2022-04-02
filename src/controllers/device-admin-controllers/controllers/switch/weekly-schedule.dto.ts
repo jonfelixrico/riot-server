c9ea5395f8d2a63a72da03afc73f89b92f52f566
Felix <jonfelixrico@gmail.com>
2022-04-02 16:26:21 +0800

    Implement validation in WeeklySchedule dto

--- src/controllers/device-admin-controllers/controllers/switch/weekly-schedule.dto.ts
+++ src/controllers/device-admin-controllers/controllers/switch/weekly-schedule.dto.ts
@@ -3,17 +3,48 @@ import {
   WeeklySchedule,
 } from '@app/services/specialized-devices/switch-manager.interface'
 import { ScheduleEntryDto } from './schedule-entry.dto'
+import { ValidateNested, Equals, IsObject, IsArray } from 'class-validator'
+
+type WeeklyScheduleSubObject = WeeklySchedule['weeklySchedule']
+
+class WeeklySchedleSubObjectDto implements WeeklyScheduleSubObject {
+  @ValidateNested()
+  @IsArray()
+  sun: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  mon: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  tues: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  wed: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  thurs: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  fri: ScheduleEntryDto[]
+
+  @ValidateNested()
+  @IsArray()
+  sat: ScheduleEntryDto[]
+}
 
 export class WeeklyScheduleDto implements WeeklySchedule {
+  @Equals('WEEKLY')
   type: 'WEEKLY'
-  weeklySchedule: {
-    sun: ScheduleEntryDto[]
-    mon: ScheduleEntryDto[]
-    tues: ScheduleEntryDto[]
-    wed: ScheduleEntryDto[]
-    thurs: ScheduleEntryDto[]
-    fri: ScheduleEntryDto[]
-    sat: ScheduleEntryDto[]
-  }
+
+  @ValidateNested()
+  @IsObject()
+  weeklySchedule: WeeklySchedleSubObjectDto
+
+  // TODO add regexp validation
   utcOffset: ScheduleUtcOffset
 }

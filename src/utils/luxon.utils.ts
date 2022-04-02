5495072e0bf79a35234774cd0cc825fabdc18a2f
Felix <jonfelixrico@gmail.com>
2022-04-02 17:47:48 +0800

    Push corrections for the logic of isValidUtc

--- src/utils/luxon.utils.ts
+++ src/utils/luxon.utils.ts
@@ -16,10 +16,30 @@ export function isBetween(date: DateTime, from: DateTime, to: DateTime) {
   return date.diff(from).milliseconds >= 0 && to.diff(date).milliseconds >= 0
 }
 
-const UTC_REGEXP = /^(?:[+-]\d\d:\d\d)|(?:[+-]\d)$/
+const UTC_SHORT = /^[+-](\d{1,2})$/
+/**
+ * We only care about checking the hours hence we don't capture minutes
+ * for the HH:MM format hence we don't capture the minutes. We let Luxon
+ * take care of the rest.
+ */
+const UTC_LONG = /^[+-](\d{2}):\d{2}$/
+
 export function isValidUtc(toCheck: string) {
-  return (
-    UTC_REGEXP.test(toCheck) &&
-    FixedOffsetZone.parseSpecifier(`UTC${toCheck}`)?.isValid
-  )
+  if (UTC_SHORT.test(toCheck)) {
+    const parsed = parseInt(UTC_SHORT.exec(toCheck)[1])
+    if (parsed > 14) {
+      return false
+    }
+
+    return FixedOffsetZone.parseSpecifier(`UTC${toCheck}`)?.isValid
+  } else if (UTC_LONG.test(toCheck)) {
+    const parsed = parseInt(UTC_LONG.exec(toCheck)[1])
+    if (parsed > 14) {
+      return false
+    }
+
+    return FixedOffsetZone.parseSpecifier(`UTC${toCheck}`)?.isValid
+  }
+
+  return false
 }

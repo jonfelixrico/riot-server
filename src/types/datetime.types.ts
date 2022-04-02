3a3886899fbc0313718a648b7593da7c5bf0058f
Felix <jonfelixrico@gmail.com>
2022-04-02 17:46:50 +0800

    Fix errors caused by the UtcSchedule type changes

--- src/types/datetime.types.ts
+++ src/types/datetime.types.ts
@@ -2,6 +2,8 @@ type SingleDigit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
 export type UtcOffset =
   | `+${SingleDigit}${SingleDigit}:${SingleDigit}${SingleDigit}`
+  | `+${SingleDigit}${SingleDigit}`
   | `+${SingleDigit}`
   | `-${SingleDigit}${SingleDigit}:${SingleDigit}${SingleDigit}`
+  | `-${SingleDigit}${SingleDigit}`
   | `-${SingleDigit}`

03dd1e521372423e15620633e7a5d80e26a3f862
Felix <jonfelixrico@gmail.com>
2022-04-02 16:15:58 +0800

    Integrate class-validator into the schedule entry DTO

--- src/controllers/device-admin-controllers/controllers/switch/schedule-entry.dto.ts
+++ src/controllers/device-admin-controllers/controllers/switch/schedule-entry.dto.ts
@@ -3,15 +3,37 @@ import {
   ScheduleTime,
   SwitchState,
 } from '@app/services/specialized-devices/switch-manager.interface'
+import { IsInt, Min, Max, IsNotEmpty, ValidateNested } from 'class-validator'
 
 export class ScheduleTimeDto implements ScheduleTime {
+  @IsInt()
+  @Min(0)
+  @Max(59)
+  @IsNotEmpty()
   minute: number
+
+  @IsInt()
+  @Min(0)
+  @Max(23)
+  @IsNotEmpty()
   hour: number
+
+  @IsInt()
+  @Min(0)
+  @Max(59)
+  @IsNotEmpty()
   second: number
 }
 
 export class ScheduleEntryDto implements ScheduleEntry {
+  @IsNotEmpty()
+  @ValidateNested()
   start: ScheduleTimeDto
+
+  @IsNotEmpty()
+  @ValidateNested()
   end: ScheduleTimeDto
+
+  // TODO integrate enum here
   state: SwitchState
 }

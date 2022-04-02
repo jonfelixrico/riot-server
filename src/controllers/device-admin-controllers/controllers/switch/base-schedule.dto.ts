649045bb3a5c8c95f46824b332b35d744317bef1
Felix <jonfelixrico@gmail.com>
2022-04-02 17:23:32 +0800

    Integrate IsUtc into base-schedule.dto.ts

--- src/controllers/device-admin-controllers/controllers/switch/base-schedule.dto.ts
+++ src/controllers/device-admin-controllers/controllers/switch/base-schedule.dto.ts
@@ -2,8 +2,10 @@ import {
   BaseSchedule,
   ScheduleUtcOffset,
 } from '@app/services/specialized-devices/switch-manager.interface'
+import { IsUtc } from '@app/validators/utc.validator'
 
 export class BaseScheduleDto implements BaseSchedule {
+  @IsUtc()
   utcOffset: ScheduleUtcOffset
   type: 'DAILY' | 'WEEKLY'
 }

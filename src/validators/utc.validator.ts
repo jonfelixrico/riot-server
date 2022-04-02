4db5da6f1e33cceb6a8bb6df41f2061a51bc5254
Felix <jonfelixrico@gmail.com>
2022-04-02 17:23:06 +0800

    Apply corrections to the IsUtc decorator

--- src/validators/utc.validator.ts
+++ src/validators/utc.validator.ts
@@ -3,13 +3,14 @@ import { FixedOffsetZone } from 'luxon'
 
 const UTC_REGEXP = /^(?:[+-]\d\d:\d\d)|(?:[+-]\d)$/
 
-export function IsUtc(property: string, validationOptions?: ValidationOptions) {
-  return function (object: Record<string, unknown>, propertyName: string) {
+export function IsUtc(validationOptions?: ValidationOptions) {
+  // We have to use Object; we can't use Record<string, unknown> (recommended) here
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
     registerDecorator({
       name: 'isUtc',
       target: object.constructor,
       propertyName: propertyName,
-      constraints: [property],
       options: validationOptions,
       validator: {
         validate(value: unknown) {

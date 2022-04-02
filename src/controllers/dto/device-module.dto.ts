d8bbc5c79c516646f7e3a5357ee82ee427f3496a
Felix <jonfelixrico@gmail.com>
2022-04-02 17:49:51 +0800

    Add validation decorators to device-module.dto

--- src/controllers/dto/device-module.dto.ts
+++ src/controllers/dto/device-module.dto.ts
@@ -1,11 +1,14 @@
 import { DeviceModule } from '@app/services/generic-devices/device-manager.interface'
 import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty } from 'class-validator'
 
 export class DeviceModuleDto implements DeviceModule {
+  @IsNotEmpty()
   moduleId: string
 
   @ApiProperty({
     enum: ['SWITCH'],
   })
+  @IsNotEmpty()
   type: string
 }

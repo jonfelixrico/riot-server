6348ec39ca9d45fc441d1686009fca0fe8010cbb
Felix <jonfelixrico@gmail.com>
2022-04-02 17:52:46 +0800

    Add decorators to devicedto

--- src/controllers/device-admin-controllers/controllers/devices/device.dto.ts
+++ src/controllers/device-admin-controllers/controllers/devices/device.dto.ts
@@ -1,9 +1,27 @@
 import { DeviceModuleDto } from '@app/controllers/dto/device-module.dto'
 import { Device } from '@app/services/generic-devices/device-manager.interface'
+import {
+  IsArray,
+  ValidateNested,
+  IsNotEmpty,
+  IsDate,
+  IsString,
+} from 'class-validator'
 
 export class DeviceDto implements Device {
+  @IsArray()
+  @ValidateNested()
   modules: DeviceModuleDto[]
+
+  @IsNotEmpty()
+  @IsString()
   firmwareVersion: string
+
+  @IsNotEmpty()
+  @IsString()
   deviceId: string
+
+  @IsNotEmpty()
+  @IsDate()
   lastHeartbeatDt: Date
 }

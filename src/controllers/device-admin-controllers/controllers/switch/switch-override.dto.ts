aa705db055bc1b8822556f7f5136b7af61e61295
Felix <jonfelixrico@gmail.com>
2022-04-02 16:35:26 +0800

    Add value to override until

--- src/controllers/device-admin-controllers/controllers/switch/switch-override.dto.ts
+++ src/controllers/device-admin-controllers/controllers/switch/switch-override.dto.ts
@@ -2,9 +2,13 @@ import {
   Override,
   SwitchState,
 } from '@app/services/specialized-devices/switch-manager.interface'
+import { IsDate, IsOptional, IsNotEmpty } from 'class-validator'
 
 export class SwitchOverrideDto implements Override {
   // TODO use an enum for this
   state: SwitchState
+
+  @IsDate()
+  @IsOptional()
   overrideUntil?: Date
 }
